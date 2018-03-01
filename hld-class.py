32daa5f6f40658bb458250ef94f845af9c9b5005
Finley <starkwafie@gmail.com>
2018-03-01 09:42:47 -0800

    code fixes and saving
    But the saving doesn’t quite work yet

--- hld-class.py
+++ hld-class.py
@@ -4,10 +4,31 @@ from PIL import Image
 import os
 
 east = os.listdir(path='hldata/east')
-eastimgs = []
+north = os.listdir(path='hldata/north')
+west = os.listdir(path='hldata/west')
+south = os.listdir(path='hldata/south')
 
-for file in east:
-	if file.endswith('.png'):
-		im = Image.open("hldata/east/" + file)
-		new = im.resize((30, 19))
-		eastimgs.append(new)
+def resizeimg(region, regionname):
+	imlist = []
+	for file in region:
+		if file.endswith('.png'):
+			im = Image.open("hldata/" + regionname + "/" + file)
+			new = im.resize((30, 19))
+			imlist.append(new)
+	return imlist
+
+eastimgs = resizeimg(east, "east")
+northimgs = resizeimg(north, "north")
+westimgs = resizeimg(west, "west")
+southimgs = resizeimg(south, "south")
+
+def saveimg(imlist, regionname):
+	count = 1
+	for img in imlist:
+		img.save("data/" + regionname + "/" + count, "JPEG")
+		count = count + 1
+
+saveimg(east, "east")
+saveimg(north, "north")
+saveimg(west, "west")
+saveimg(south, "south")
