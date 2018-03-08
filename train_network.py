6ed75a5496561b3e37bd8e4720df4a5b1b9e323c
Finley <starkwafie@gmail.com>
2018-03-08 09:47:25 -0800

    beginning to use an online tutorial

---
+++ train_network.py
@@ -0,0 +1,100 @@
+# https://www.pyimagesearch.com/2017/12/11/image-classification-with-keras-and-deep-learning/
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+ 
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from pyimagesearch.lenet import LeNet
+from imutils import paths
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import cv2
+import os
+
+
+east = os.listdir(path='hldata/east')
+north = os.listdir(path='hldata/north')
+west = os.listdir(path='hldata/west')
+south = os.listdir(path='hldata/south')
+
+def toarray(region, regionname):
+	imlist = []
+	for file in region:
+		if file.endswith('.jpg'):
+			im = Image.open("hldata/" + regionname + "/" + file)
+			imlist.append(np.array(im))
+	return imlist
+
+eastlist = toarray(east, "east")
+northlist = toarray(north, "north")
+westlist = toarray(west, "west")
+southlist = toarray(south, "south")
+
+#data = eastlist + northlist + westlist + southlist
+
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
+
+# initialize the number of epochs to train for, initial learning rate,
+# and batch size
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+ 
+# initialize the data and labels
+print("[INFO] loading images...")
+data = []
+labels = []
+ 
+# grab the image paths and randomly shuffle them
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+
+# loop over the input images
+for imagePath in imagePaths:
+	# load the image, pre-process it, and store it in the data list
+	image = cv2.imread(imagePath)
+	image = cv2.resize(image, (28, 28))
+	image = img_to_array(image)
+	data.append(image)
+ 
+	# extract the class label from the image path and update the
+	# labels list
+	label = imagePath.split(os.path.sep)[-1]
+	label = [1, 0, 0, 0] if label == "east"
+	label = [0, 1, 0, 0] if label == "north"
+	label = [0, 0, 1, 0] if label == "west"
+	label = [0, 0, 0, 1] if label == "south"
+	labels.append(label)
+
+# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+ 
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.25, random_state=42)
+ 
+# convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
