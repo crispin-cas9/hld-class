737327c9de4aa953c1110eca6d15ba0a9ca9e679
Finley <starkwafie@gmail.com>
2018-03-12 12:19:51 -0700

    Small tweaks

--- train_network.py
+++ train_network.py
@@ -10,7 +10,7 @@ from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
 from keras.utils import to_categorical
-from pyimagesearch.lenet import LeNet
+from lenet import LeNet
 from imutils import paths
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -80,10 +80,14 @@ for imagePath in imagePaths:
 	# extract the class label from the image path and update the
 	# labels list
 	label = imagePath.split(os.path.sep)[-1]
-	label = [1, 0, 0, 0] if label == "east"
-	label = [0, 1, 0, 0] if label == "north"
-	label = [0, 0, 1, 0] if label == "west"
-	label = [0, 0, 0, 1] if label == "south"
+	if label == "east":
+		label = [1, 0, 0, 0] 
+	if label == "north":
+		label = [0, 1, 0, 0]
+	if label == "west":
+		label = [0, 0, 1, 0]
+	if label == "south":
+		label = [0, 0, 0, 1]
 	labels.append(label)
 
 # scale the raw pixel intensities to the range [0, 1]
@@ -98,3 +102,9 @@ labels = np.array(labels)
 # convert the labels from integers to vectors
 trainY = to_categorical(trainY, num_classes=2)
 testY = to_categorical(testY, num_classes=2)
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")
+
